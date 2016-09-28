22043cce1791ea36f84cc130891ba00489a188cb
Stephanie Cruz <cruz.stephanie2294@gmail.com>
2016-09-27 23:42:03 -0400

    created initial maven project

---
+++ dev/src/main/java/exscruzme/App.java
@@ -0,0 +1,13 @@
+package exscruzme;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hey World! This is my climbing data app!" );
+    }
+}
