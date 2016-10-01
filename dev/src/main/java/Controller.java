18bdcdd9d3e834a614c5d12936ad4a1e5e1b6ffb
Stephanie Cruz <cruz.stephanie2294@gmail.com>
2016-10-01 15:14:30 -0400

    ok for real i think this is configured correctly

---
+++ dev/src/main/java/Controller.java
@@ -0,0 +1,10 @@
+package src.main.java;
+
+/**
+ * Created by stephaniecruz on 10/1/16.
+ */
+public class Controller {
+    public static void main(String [] args) {
+        System.out.println("Help me plz");
+    }
+}
